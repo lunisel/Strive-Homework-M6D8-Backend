bfb05e00b241c6c60bdee424f6246fe9973bf1ea
Luna Sarmiento <selene942008@hotmail.com>
2021-09-01 16:46:56 +0200

    comments

--- src/blogs/index.js
+++ src/blogs/index.js
@@ -48,4 +48,75 @@ blogRouter.put("/:id", async (req, resp, next) => {
   }
 });
 
+blogRouter.delete("/:id", async (req, resp, next) => {
+  try {
+    const blog = await Blogs.findById(req.params.id);
+    if (!blog) {
+      resp
+        .status(404)
+        .send({ message: `blog with ${req.params.id} is not found!` });
+    } else {
+      await Blogs.findByIdAndDelete(req.params.id);
+      resp.status(204).send();
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+blogRouter.get("/:id/comments", async (req, resp, next) => {
+  try {
+    const blog = await Blogs.findById(req.params.id);
+    if (blog) {
+      resp.send(blog.comments);
+    } else {
+      resp
+        .status(404)
+        .send({ message: `blog with ${req.params.id} is not found!` });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+blogRouter.post("/:id/comments", async (req, resp, next) => {
+  try {
+    const comment = req.body;
+    const updatedBlog = await Blogs.findByIdAndUpdate(
+      req.params.id,
+      { $push: { comments: comment } },
+      { new: true }
+    );
+    if (updatedBlog) {
+      resp.send(updatedBlog);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+blogRouter.get("/:id/comments/:commentId", async (req, resp, next) => {
+  try {
+    const blog = await Blogs.findById(req.params.id);
+    if (blog) {
+      const comment = await blog.comments.find(
+        (c) => c._id.toString() === req.params.commentId
+      );
+      if (comment) {
+        resp.send(comment);
+      } else {
+        resp
+          .status(404)
+          .send({ message: `Comment with ${req.params.commentId} not found!` });
+      }
+    } else {
+      resp
+        .status(404)
+        .send({ message: `Blog with ${req.params.id} is not found!` });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
 export default blogRouter;
